e30c38f0002a600eb4bb446e9c2a92d0cf94a35c
Evgeniy <eurbagayev@mail.com>
2022-09-23 11:26:17 +0300

    На входе трехначное число, на выходе вторая цифра числа

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите трехзначное число:  ");
+int number = Convert.ToInt32(ReadLine());
+
+if(number >= 100)
+{
+if(number <= 999)
+{
+    int num = number / 10;
+    int num1 = num % 10;
+    WriteLine($"{num1}");
+}
+}
+else
+{
+    WriteLine($"Введите число больше 100 и меньше 999");
+}

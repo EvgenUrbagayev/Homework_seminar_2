68ff36a00baa5b2e022a080ea626909fec8216e4
Evgeniy <eurbagayev@mail.com>
2022-09-24 13:10:01 +0300

    Вывод третьей цифры числа

---
+++ Task_13/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введите трехзначное число:  ");
+int num = Convert.ToInt32(ReadLine());
+
+if (num < 100)
+{
+    WriteLine("Третьей цифры нет");
+}
+
+if(num > 99 && num < 999)
+{
+    int num1 = num % 10;
+    WriteLine(num1);
+}
+
+if (num > 999 && num < 9999)
+{
+    int num1 = (num % 100) / 10;
+    WriteLine(num1);
+}
+
+if (num > 9999 && num < 99999)
+{
+    int num1 = (((num % 1000) / 10) / 10);
+    WriteLine(num1);
+}
